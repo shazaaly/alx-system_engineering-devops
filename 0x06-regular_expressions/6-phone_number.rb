eb0c1f213083fff80f54db6e918a9e335b2993ac
shazaaly <shaza.aly@gmail.com>
2023-08-02 02:01:48 +0300

    6-phone_number.rb

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+# a Ruby script that accepts one argument and pass it
+# exactly matching a string that starts with h ends with n and can have any single character in between
+
+puts ARGV[0].scan(/\d{10}/).join
