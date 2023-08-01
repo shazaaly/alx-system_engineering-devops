c10c3c5de9f411b5608e0061c4e4aacee46f396e
shazaaly <shaza.aly@gmail.com>
2023-08-02 01:17:13 +0300

    fix repetition as low as 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,4 +2,4 @@
 # a Ruby script that accepts one argument and pass it
 # to a regular expression matching using  repetition topens
 
-puts ARGV[0].scan(/h{1}b{1}t{2,5}n{1}/).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
