f2143849b1126068c222d77c5e00c20b9326bf34
shazaaly <shaza.aly@gmail.com>
2023-08-02 01:27:28 +0300

    quantifier

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -2,4 +2,4 @@
 # a Ruby script that accepts one argument and pass it
 # to a regular expression matching using  repetition topens
 
-puts ARGV[0].scan(/hbt{1,4}n/).join
+puts ARGV[0].scan(/hbt{1,}n/).join
