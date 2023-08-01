3b85fc6114958fc5b6375c1d39b111937783f725
shazaaly <shaza.aly@gmail.com>
2023-08-02 01:37:06 +0300

    astrisk repetition token

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -2,4 +2,4 @@
 # a Ruby script that accepts one argument and pass it
 # to a regular expression matching using  repetition topens
 
-puts ARGV[0].scan(/hbt{0,4}n/).join
+puts ARGV[0].scan(/hbt*n/).join
