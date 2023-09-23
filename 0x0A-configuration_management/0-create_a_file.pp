b788536e5822ac9095b990e3dce84f91a2b0b310
shazaaly <shaza.aly@gmail.com>
2023-09-23 05:31:02 +0300

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,11 @@
 # Puppet Manifest to Create and Configure a File
 
 file{ '/tmp/school':
-ensure => 'file',
-permission => 0744,
-owner => 'www-data'
-group => 'www-data'
-content => 'I love Puppet'
+ensure  => 'file',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 
 }
+
