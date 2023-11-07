e8a9b1dd143a9e9c8b80481ec122bceacc4020bd
shazaaly <shaza.aly@gmail.com>
2023-11-08 01:42:02 +0200

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script to get top 10 hot posts on a subreddit"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """a function that queries the Reddit API and prints
+    the titles of the first 10 hot
+    posts listed for a given subreddit."""
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=9"
+    response = requests.get(url, headers={'User-Agent': 'app/1.0'})
+    data = response.json()
+
+    # Print the response data
+    posts = data['data']['children']
+    for post in posts:
+        print(post['data']['title'])
+
+# if __name__ == '__main__':
+#     # Example: Get the response structure for the "programming" subreddit
+#     subreddit_name = 'programming'
+#     top_ten(subreddit_name)
